5742f744ba71196a1e54ec5140d0ac1f31fa5f04
Ryan Cain <racain05@gmail.com>
2022-05-03 20:47:13 -0400

    implemented search functionality

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -4,7 +4,7 @@ import GithubContext from '../../context/github/GithubContext'
 function UserSearch() {
   const [text, setText] = useState('')
 
-  const {users} = useContext(GithubContext);
+  const {users, searchUsers} = useContext(GithubContext);
   
   const handleChange = (e) => {
     setText(e.target.value)
@@ -15,8 +15,7 @@ function UserSearch() {
       if (text === '') {
           alert('Please enter something')
       } else {
-          // @todo - search users
-
+          searchUsers(text)
           setText('')
       }
   }
@@ -28,7 +27,7 @@ function UserSearch() {
                 <div className="form-control">
                     <div className="relative">
                         <input type="text" className="w-full pr-40 bg-gray-200 input input-lg text-center" placeholder="Search" value={text} onChange={handleChange} />
-                        <button type="submit" className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg"></button>
+                        <button type="submit" className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg">Submit</button>
                     </div>
                 </div>
             </form>
