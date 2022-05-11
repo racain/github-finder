ce460524104fd2b68d1596c7cec755bd844891a6
Ryan Cain <racain05@gmail.com>
2022-05-11 05:33:16 -0400

    started user profile page layout

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -41,12 +41,35 @@ function User() {
           <Link to='/' className='btn btn-ghost'>Back to Search</Link>
         </div>
 
-        <div className="grid-grid-cols-1 xl:grid-cols-3 lg:cols-cols-3 md:grid-cols-3 mb-8 md:gap-8">
+        <div className="grid grid-cols-1 xl:grid-cols-3 lg:cols-cols-3 md:grid-cols-3 mb-8 md:gap-8">
           <div className="custom-card-image mb-6 md:mb-0">
             <div className="rounded-lg shadow-xl card image-full">
               <figure>
                 <img src={avatar_url} alt={name} />
               </figure>
+              <div className="card-body justify-end">
+                <h2 className="card-title mb-0">
+                  {name}
+                </h2>
+                <p>{login}</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-2">
+            <div className="mb-6">
+              <h1 className="text-3xl card-title">
+                {name}
+                <div className="ml-2 mr-1 badge badge-success">
+                  {type}
+                </div>
+                {hireable && (
+                  <div className="mx-1 badge badge-info">Hireable</div>
+                )}
+              </h1>
+              <p>{bio}</p>
+              <div className="mt-4 card-actions">
+                <a href={html_url} target='_blank' rel='noreferrer' className='btn btn-outline'>Visit Github Profile</a>
+              </div>
             </div>
           </div>
         </div>
