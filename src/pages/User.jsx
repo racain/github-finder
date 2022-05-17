d6df9df79d5f832308b1182c38047c14c80fd210
Ryan Cain <racain05@gmail.com>
2022-05-17 05:13:53 -0400

    cleaned up githubContext file and relocated getUser and getUserRepos to githubActions folder

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,14 +5,23 @@ import { FaCodepen, FaStore, FaUserFriends, FaUsers} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import RepoList from '../components/repos/RepoList'
 import Spinner from '../components/assets/Spinner'
+import {getUser, getUserRepos} from '../context/github/GithubActions'
 
 function User() {
-  const {getUser, user, loading, getUserRepos, repos} = useContext(GithubContext)
+  const {user, loading, repos, dispatch} = useContext(GithubContext)
   const params = useParams()
 
   useEffect( () => {
-    getUser(params.login)
-    getUserRepos(params.login)
+    dispatch({type: 'SET_LOADING'})
+    const getUserData = async () => {
+      const userData = await getUser(params.login)
+      dispatch({type: 'GET_USER', payload: userData})
+
+      const userRepoData = await getUserRepos(params.login)
+      dispatch({type: 'GET_REPOS', payload: userRepoData})
+    }
+
+    getUserData()
   }, [])
 
   const {
